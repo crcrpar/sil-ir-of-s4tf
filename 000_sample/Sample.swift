c5969fa8ba1bf79adb7a6b798c5217369631269a
crcrpar <masaki.kozuki.2014@gmail.com>
2019-07-31 18:45:09 +0900

    update

--- 000_sample/Sample.swift
+++ 000_sample/Sample.swift
@@ -1,11 +1,10 @@
+import Foundation
 import TensorFlow
 
+func main() {
+    let x = Tensor<Float>(randomNormal: [1, 32, 32, 3])
+    let filter = Tensor<Float>(randomUniform: [3, 3, 3, 16])
 
-let x = Tensor<Float>(randomNormal: [1, 32, 32, 3])
-let filter = Tensor<Float>(randomNormal: [3, 3, 3, 16])
-for i in 1...100 {
     let out = x.convolved2D(withFilter: filter, strides: (1, 1, 1, 1), padding: Padding.same)
-    if i % 10 == 0 {
-        print("\(i) th convolution. out.shape = \(out.shape)")
-    }
+    print("\(x.shape), \(out.shape)")
 }
